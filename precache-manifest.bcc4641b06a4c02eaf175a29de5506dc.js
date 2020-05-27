fa0decdaface02d1ba3ba941db34c1b5011b4991
Elijah <igundor@wgu.edu>
2020-05-27 17:25:08 -0400

    Updates

---
+++ precache-manifest.bcc4641b06a4c02eaf175a29de5506dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e367b644f5160adfe80427e98fa3d64c",
+    "url": "/Minion-Search/index.html"
+  },
+  {
+    "revision": "123b85a0f2a915e29676",
+    "url": "/Minion-Search/static/css/main.8526aee3.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/Minion-Search/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Minion-Search/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123b85a0f2a915e29676",
+    "url": "/Minion-Search/static/js/main.158168e2.chunk.js"
+  },
+  {
+    "revision": "c762caa6750f7cd4428f",
+    "url": "/Minion-Search/static/js/runtime-main.c879791d.js"
+  }
+]);
